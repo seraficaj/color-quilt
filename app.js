eda01fc31355a5ef854a8bcc5d2dc4c7513e7c6c
Jason Serafica <seraficaj@gmail.com>
2022-02-02 11:45:34 -0800

    give random colors to squares

--- app.js
+++ app.js
@@ -10,19 +10,31 @@ const changeH2 = () => {
     document.getElementsByTagName("h2")[0].innerText = "Getting warmed up!";
 };
 
+const randColorRGB = () => {
+    const red = Math.floor(Math.random()*256);
+    const green = Math.floor(Math.random()*256);
+    const blue = Math.floor(Math.random()*256);
+    return `rgb(${red}, ${green}, ${blue})`;
+}
+
+const generateQuilt = (numOfSquares) => {
+    // generate numOfSquares divs with a loop
+    for (let i = 1; i <= numOfSquares; i++) {
+        console.log(i);
+        // create a new DOM element
+        let square = document.createElement("div");
+        // give the div the css class "square"
+        square.classList.add("square");
+        // add a randomly generated color
+        square.style.backgroundColor = randColorRGB();
+        // append it to the page
+        document.querySelector("body").appendChild(square);
+    }
+};
+
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Bonjour! from your JS file");
     addH2();
     changeH2();
-    // generate 100 divs with a loop
-    for (let i = 1; i <= 100; i++) {
-        console.log(i);
-        // create a new DOM element
-        let square = document.createElement('div');
-        // give the div the css class "square"
-        square.classList.add('square');
-        // append it to the page
-        square.innerText = i;
-        document.querySelector('body').appendChild(square);
-    }
+    generateQuilt(10);
 });
